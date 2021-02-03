5fae186fd836df59106db5c6bd8b6b60ebd112a4
maria-canals <mariacanals23@gmail.com>
2021-02-03 21:09:54 +0100

    refactorized

--- appvue.js
+++ appvue.js
@@ -2,33 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             inputNumber: "",
-            show : false
         }
     },
-    mounted() {
-        this.show = true
-    },
+
     methods: {
+        getInput(event){
+          this.inputNumber += event.target.dataset.num
+        },
         clearAll(){
             this.inputNumber= "" 
-        },
-        // f() que ens concatenaran els valors dels numeros o els simbols clickats amb el que ja existeixi 
-        // dins de  inputNumber (linea4)
-        numbersChoice(n){
-            this.inputNumber = `${this.inputNumber}${n}`
-        },
-        plusButton(n){
-            this.inputNumber = `${this.inputNumber}${n}`
-        },
-        reduceButton(n){
-            this.inputNumber = `${this.inputNumber}${n}`
-        },
-        multButton(n){
-            this.inputNumber = `${this.inputNumber}${n}`
-        },
-        divButton(n){
-            this.inputNumber = `${this.inputNumber}${n}`
-        },          
+        },       
         equal(){
             return this.inputNumber = eval(this.inputNumber)
         }
